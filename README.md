803f6398ac28e3ef1d92f980d7fd2e654fb23ec9
Pierricles <pierromanuel5@gmail.com>
2019-11-14 13:38:10 +0100

    Creation d'un reseau routier

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sonification
-ISEN Project Sonification
--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-import requests
-import json
+import node
+import road
+import Graph
 
-class Point:
-    latitude=0
-    longitude=0
-    def __init__(self,latitude=0, longitude=0):
-        self.latitude = latitude
-        self.longitude = longitude
+my_graph=Graph.Graph()
+my_graph.create_rand(10)
+print("0")
 
-r = requests.get("https://www.mapquestapi.com/directions/v2/alternateroutes?key=uMb0YEdtislXWuR2w0rt1hk11NQXjA4p&from=Lille%2C+FR&to=Villeneuve+d'Ascq%2C+FR&outFormat=json&ambiguities=check&routeType=fastest&maxRoutes=10&timeOverage=25&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false&unit=M")
-data = r.json()
-json.dumps(data, indent = 4, sort_keys=True)
 
-p = (data["route"]["alternateRoutes"][0]["route"]["shape"]["shapePoints"])
-nodeTable=[]
-while len(p) != 0:
-    point = Point()
-    point.latitude = p.pop()
-    point.longitude = p.pop()
-    nodeTable.append(point)
-print(nodeTable[0].latitude)
+
+
+
