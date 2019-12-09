30bd99223df57b03fe0ef599bd367c93d234fa2b
Pierricles <pierromanuel5@gmail.com>
2019-12-09 10:00:54 +0100

    exportateur en txt

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import road
 import Graph
 import Plus_cour_chemin as Dj
 
-def find_fastest(my_graph,debut,fin):
-    size, way = Dj.dij_rec(my_graph.network,debut,fin)
-    if size != False :
+
+def find_fastest(my_graph, debut, fin):
+    size, way = Dj.dij_rec(my_graph.network, debut, fin)
+    if size != False:
         path_node = []
         path_roade = []
         for i in range(len(way)):
@@ -16,33 +17,35 @@ def find_fastest(my_graph,debut,fin):
             if way[i] != fin:
                 path_roade.append(my_graph.network[way[i]][way[i + 1]])
 
-        return path_node, path_roade,size
-    else: return False,False,False
+        return path_node, path_roade, size
+    else:
+        return False, False, False
+
 
 def find_min(All_way):
-    min=4*[0]
-    min[2]=10000
+    min = 4 * [0]
+    min[2] = 10000
     for i in All_way:
-        if i[2]<min[2] and i[3]:
-            min =deepcopy(i)
-    min[3]=False
+        if i[2] < min[2] and i[3]:
+            min = deepcopy(i)
+    min[3] = False
     return i
 
 
-def isin(All_way,way):
+def isin(All_way, way):
     for i in All_way:
-        if i[0]==way:
+        if i[0] == way:
             return True
 
 
-my_graph=Graph.Graph()
-All_way=[]
+my_graph = Graph.Graph()
+All_way = []
 my_graph.create_rand(100)
-debut=0
-fin=9
+debut = 0
+fin = 9
 
-i=0
-a, best,size = find_fastest(my_graph, debut, fin)
+i = 0
+a, best, size = find_fastest(my_graph, debut, fin)
 if a != False:
     All_way.append([])
     All_way[i].append(a)
@@ -65,15 +68,45 @@ if a != False:
                 All_way[i].append(True)
                 i += 1
 
-else :print ("no way")
+else:
+    print("no way")
 
+fichier = open("data_way.txt", "w")
 
+for i in All_way:
+    total_length = 0
+    mean_speed = 0
+    total_trafficJamsSize = 0
+    mean_trafficJamsSpeed = 0
+    total_accident = 0
+    mean_winding = 0
+    total_toll = 0
+    wild = False
+    total_stop = 0
+    total_tunnel = 0
+    total_bridge = 0
+    mean_montaine = 0
+    for j in i[1]:
+        total_length += j.length
+        mean_speed += j.maxSpeed * j.length
+        total_trafficJamsSize += j.trafficJamsSize
+        mean_trafficJamsSpeed += j.trafficJamsSpeed
+        total_accident += j.accident
+        mean_winding += j.winding * j.length
+        total_toll += j.toll
+        total_stop += j.stop
+        total_tunnel += j.tunnel
+        total_bridge += j.tunnel
+        mean_montaine += j.montaine * j.length
+    mean_speed = mean_speed / total_length
+    mean_trafficJamsSpeed = mean_trafficJamsSpeed / total_trafficJamsSize
+    mean_winding = mean_winding / total_length
+    mean_montaine = mean_montaine / total_length
+    fichier.write(
+        str(round(total_length)) + " " + str(round(mean_speed)) + " " + str(round(total_trafficJamsSize)) + " " + str(
+            round(mean_trafficJamsSpeed)) + " " + str(round(total_accident)) + " " + str(
+            round(mean_winding)) + " " + str(round(total_toll)) + " " + str(round(total_stop)) + " " + str(
+            round(total_tunnel)) + " " + str(round(total_bridge)) + " " + str(round(mean_montaine))+"\n")
 
-
+fichier.close()
 print(0)
-
-
-
-
-
-
