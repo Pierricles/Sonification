7c6c6252880bdabd88ef61c093c4c4770335a4e4
Pierricles <pierromanuel5@gmail.com>
2019-12-20 10:53:15 +0100

    comment

--- Yen.py
+++ Yen.py
@@ -1,8 +1,9 @@
 from copy import deepcopy
 
 import Plus_cour_chemin as Dj
+# algorithme de Yen
 
-def find_fastest(my_graph, debut, fin):
+def find_fastest(my_graph, debut, fin):# fonction qui renvoit le chemin de noeud trouve par Dijkstra
     size, way = Dj.dij_rec(my_graph.network, debut, fin)
     if size != False:
         path_node = []
@@ -18,117 +19,104 @@ def find_fastest(my_graph, debut, fin):
 
 
 def YenKSP(my_graph, source, sink, K):
-    old_graph=deepcopy(my_graph)
-    way = find_fastest(my_graph,source, sink)
-    a=[]
+    way = find_fastest(my_graph, source, sink) # on cherche la premiere route la plus rapide
+    a = []
     a.append(way)
-    b=[]
-    remove_edge=[]
-    for k in range(1,K):
-        for i in range(0,len(a[k-1])-1):
-            spur_node=a[k-1][i]
-            root_path=[]
-            for node in range(len(a[k-1])):
-                if node<=i:
-                    root_path.append(a[k-1][node])
+    b = []
+    remove_edge = []
+    for k in range(1, K):
+        for i in range(0, len(a[k - 1]) - 1):
+            spur_node = a[k - 1][i]  # spur_node est récupéré à partir du chemin k le plus court précédent, k - 1.
+            root_path = [] #les noeuds de la source au spur_node du  chemin le plus court précédent.
+            for node in range(len(a[k - 1])):
+                if node <= i:
+                    root_path.append(a[k - 1][node])
 
             for p in a:
-                isroot_path=True
-                for j in range(0,i):
+                #Supprime les liens qui font partie des chemins plus cours précédents et partage la meme recine
+                isroot_path = True
+                for j in range(0, i):
 
-                    if j>=len(p):
+                    if j >= len(p):
                         isroot_path = False
-                    elif root_path[j]!=p[j]:
-                        isroot_path=False
-                if isroot_path and p[i].name!=sink:
-                    if my_graph.network[p[i].name].get(p[i + 1].name,False):
+                    elif root_path[j] != p[j]:
+                        isroot_path = False
+                if isroot_path and p[i].name != sink:
+                    if my_graph.network[p[i].name].get(p[i + 1].name, False):
                         remove_edge.append(my_graph.network[p[i].name][p[i + 1].name])
                         del my_graph.network[p[i].name][p[i + 1].name]
                     if my_graph.network[p[i + 1].name].get(p[i].name, False):
                         remove_edge.append(my_graph.network[p[i + 1].name][p[i].name])
-                        del my_graph.network[p[i+1].name][p[i].name]
-
+                        del my_graph.network[p[i + 1].name][p[i].name]
 
             for root_path_node in root_path:
-                if root_path_node!=spur_node :
+                if root_path_node != spur_node:
                     for node in my_graph.network:
-                        if my_graph.network[root_path_node.name].get(node,False):
+                        if my_graph.network[root_path_node.name].get(node, False):
                             remove_edge.append(my_graph.network[root_path_node.name][node])
                             del my_graph.network[root_path_node.name][node]
 
-                        if my_graph.network[node].get(root_path_node.name,False):
+                        if my_graph.network[node].get(root_path_node.name, False):
                             remove_edge.append(my_graph.network[node][root_path_node.name])
                             del my_graph.network[node][root_path_node.name]
 
-
-            spur_path=find_fastest(my_graph,spur_node.name, sink)
-            if spur_path==False:
+            spur_path = find_fastest(my_graph, spur_node.name, sink) # on calcule un nouveau chemin
+            if spur_path == False:
                 print("no way")
-            total_path=root_path+spur_path
+            total_path = root_path + spur_path
             del total_path[i]
             b.append(total_path)
             for h in remove_edge:
-                my_graph.network[h.start][h.end]=h
-            remove_edge=[]
+                my_graph.network[h.start][h.end] = h
+            remove_edge = []
         if len(b) == 0:
             break
-        quick_sort(b,0,len(b)-1,my_graph.network)
+        quick_sort(b, 0, len(b) - 1, my_graph.network)
         a.append(b[0])
-        suppr=b[0]
-        i=0
+        suppr = b[0]
+        i = 0
         while i < len(b):
             if b[i] == suppr:
                 del b[i]
-            else :i+=1
-
-
+            else:
+                i += 1
 
     return a
 
-def partition(array, start, end,graph):
-    pivot = calc_length(array[start],graph)
+
+def partition(array, start, end, graph):# sous fonction de tri
+    pivot = calc_length(array[start], graph)
     low = start + 1
     high = end
 
     while True:
-        # If the current value we're looking at is larger than the pivot
-        # it's in the right place (right side of pivot) and we can move left,
-        # to the next element.
-        # We also need to make sure we haven't surpassed the low pointer, since that
-        # indicates we have already moved all the elements to their correct side of the pivot
-        while low <= high and calc_length(array[high],graph) >= pivot:
+
+        while low <= high and calc_length(array[high], graph) >= pivot:
             high = high - 1
-
-        # Opposite process of the one above
-        while low <= high and calc_length(array[low],graph)<= pivot:
+        while low <= high and calc_length(array[low], graph) <= pivot:
             low = low + 1
-
-        # We either found a value for both high and low that is out of order
-        # or low is higher than high, in which case we exit the loop
         if low <= high:
             array[low], array[high] = array[high], array[low]
-            # The loop continues
         else:
-            # We exit out of the loop
             break
-
     array[start], array[high] = array[high], array[start]
-
     return high
 
-def quick_sort(array, start, end,graph):
+
+def quick_sort(array, start, end, graph): # fonction de tri
     if start >= end:
         return
+    p = partition(array, start, end, graph)
+    quick_sort(array, start, p - 1, graph)
+    quick_sort(array, p + 1, end, graph)
 
-    p = partition(array, start, end,graph)
-    quick_sort(array, start, p-1,graph)
-    quick_sort(array, p+1, end,graph)
 
-def calc_length(way,graph):
-    length=0
+def calc_length(way, graph):# calcule la longeure du trajet
+    length = 0
     for i in range(len(way[:-1])):
         try:
-            length+=graph[way[i].name][way[i+1].name].length
-        except:print('error')
+            length += graph[way[i].name][way[i + 1].name].length
+        except:
+            print('error')
 
     return length
--- main.py
+++ main.py
@@ -1,88 +1,24 @@
-import random
-from copy import deepcopy
 
-import node
-import road
 import Graph
-import Plus_cour_chemin as Dj
 from Yen import YenKSP
 
 
-
-def find_fastest(my_graph, debut, fin):
-    size, way = Dj.dij_rec(my_graph.network, debut, fin)
-    if size != False:
-        path_node = []
-        path_roade = []
-        for i in range(len(way)):
-            path_node.append(my_graph.node[way[i]])
-            if way[i] != fin:
-                path_roade.append(my_graph.network[way[i]][way[i + 1]])
-
-        return path_node, path_roade, size
-    else:
-        return False, False, False
-
-
-def find_min(All_way):
-    min = 4 * [0]
-    min[2] = 10000
-    for i in All_way:
-        if i[2] < min[2] and i[3]:
-            min = deepcopy(i)
-    min[3] = False
-    return min
-
-
-def isin(All_way, way):
-    for i in All_way:
-        if i[0] == way:
-            return True
-
-
 my_graph = Graph.Graph()
-All_way = []
-my_graph.create_rand(100)
+my_graph.create_rand(100)#generation d'un graph de 100 noeuds
 debut = 0
 fin = 9
-path=YenKSP(my_graph, debut, fin,K=10)
+path=YenKSP(my_graph, debut, fin,K=10) # algorithme de Yen
 path_all_road=[]
-for j in path:
+for j in path:# creation d'un chemin d'arc a partire d'un chemin de noeuds
     path_roade = []
     for i in range(len(j)):
         if j[i].name != fin:
             path_roade.append(my_graph.network[j[i].name][j[i + 1].name])
     path_all_road.append(path_roade)
 
-'''
-i = 0
-a, best, size = find_fastest(my_graph, debut, fin)
-if a != False:
-    All_way.append([])
-    All_way[i].append(a)
-    All_way[i].append(best)
-    All_way[i].append(size)
-    All_way[i].append(True)
-    i += 1
-    while i < 20:
-        best = find_min(All_way)
-        for r in best[1]:
-            new_graph = deepcopy(my_graph)
-            del new_graph.network[r.end][r.start]
-            del new_graph.network[r.start][r.end]
-            a, b, size = find_fastest(new_graph, debut, fin)
-            if a != False and not isin(All_way, a):
-                All_way.append([])
-                All_way[i].append(a)
-                All_way[i].append(b)
-                All_way[i].append(size)
-                All_way[i].append(True)
-                i += 1
 
-else:
-    print("no way")
-'''
-fichier = open("data_way.txt", "w")
+fichier = open("data_way.txt", "w")# ouverture du fichier pour l'ecriture
+
 
 for i in path_all_road:
     total_length = 0
@@ -114,10 +50,8 @@ for i in path_all_road:
     mean_winding = mean_winding / total_length
     mean_montaine = mean_montaine / total_length
     fichier.write(
-        str(round(total_length)) + " " + str(round(mean_speed)) + " " + str(round(total_trafficJamsSize)) + " " + str(
-            round(mean_trafficJamsSpeed)) + " " + str(round(total_accident)) + " " + str(
-            round(mean_winding)) + " " + str(round(total_toll)) + " " + str(round(total_stop)) + " " + str(
-            round(total_tunnel)) + " " + str(round(total_bridge)) + " " + str(round(mean_montaine))+"\n")
+        str(round(total_length)) + " " + str(round(mean_speed/total_length)) + " " + str(round(total_trafficJamsSize)) + " " + str(round(total_toll))  + " " + str(
+            round(total_tunnel)) +"\n")
 
 fichier.close()
 print(0)
